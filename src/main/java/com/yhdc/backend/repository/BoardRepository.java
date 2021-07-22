4c4c368675aedcb9b4f1c27d59101817d492577c
yhdchoi <yhdchoi@gmail.com>
2021-07-22 22:08:29 +0900

    Update

--- src/main/java/com/yhdc/backend/repository/BoardRepository.java
+++ src/main/java/com/yhdc/backend/repository/BoardRepository.java
@@ -13,19 +13,19 @@ import com.yhdc.backend.repository.search.SearchBoardRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, SearchBoardRepository {
 
-	// Board with Writer
-		@Query("SELECT b, w FROM Board b LEFT JOIN b.writer w WHERE b.bno = :bno")
-		Object getBoardWithWriter(@Param("bno") Long bno);
+	// GET Board with Writer
+	@Query("SELECT b, w FROM Board b LEFT JOIN b.writer w WHERE b.bno = :bno")
+	Object getBoardWithWriter(@Param("bno") Long bno);
 
-		// Board with Reply
-		@Query("SELECT b, c FROM Board b LEFT JOIN Comment c ON c.board = b WHERE b.bno = :bno")
-		List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+	// Get Board with Reply
+	@Query("SELECT b, c FROM Board b LEFT JOIN Comment c ON c.board = b WHERE b.bno = :bno")
+	List<Object[]> getBoardWithReply(@Param("bno") Long bno);
 
-		// Board with ReplyCount
-		@Query(value = "SELECT b, w, count(c) FROM Board b LEFT JOIN b.writer w LEFT JOIN Comment c ON c.board = b GROUP BY b", countQuery = "SELECT count(b) FROM Board b")
-		Page<Object[]> getBoardWithReplyCount(Pageable pageable);
+	// Board with ReplyCount
+	@Query(value = "SELECT b, w, count(c) FROM Board b LEFT JOIN b.writer w LEFT JOIN Comment c ON c.board = b GROUP BY b", countQuery = "SELECT count(b) FROM Board b")
+	Page<Object[]> getBoardWithReplyCount(Pageable pageable);
 
-		// Get with BoardID
-		@Query("SELECT b, w, count(c) FROM Board b LEFT JOIN b.writer w LEFT OUTER JOIN Comment c ON c.board = b WHERE b.bno = :bno")
-		Object getBoardByBno(@Param("bno") Long bno);
+	// Get with BoardID
+	@Query("SELECT b, w, count(c) FROM Board b LEFT JOIN b.writer w LEFT OUTER JOIN Comment c ON c.board = b WHERE b.bno = :bno")
+	Object getBoardByBno(@Param("bno") Long bno);
 }
